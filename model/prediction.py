e1860312c7b578919d0ed7e8a36c8536f9248abd
Asma <asmabrazi27@gmail.com>
2021-01-28 09:45:30 +0100

    RGB image

--- model/prediction.py
+++ model/prediction.py
@@ -178,9 +178,10 @@ def localize(net,im):
     
     contours,_ = cv.findContours(im_gray, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     cv.drawContours(contour, contours, -1, (0,0,255), 3)
+    im_rgb = cv2.cvtColor(contour, cv2.COLOR_BGR2RGB)
     #cv.imwrite("BB"+str(i)+".png",contour)
     
-    return contour
+    return im_rgb
 
 def classify(net,im):
     i  = 1
