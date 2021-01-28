17538970733361dad3516037398761ce67912394
Asma <asmabrazi27@gmail.com>
2021-01-28 09:31:48 +0100

    read image

--- model/prediction.py
+++ model/prediction.py
@@ -176,7 +176,7 @@ def localize(net,im):
     
     contour = im.copy()
     
-    contours,_ = cv.findContours(im_gray, cv.RETR_TREE, cv.CHAIN  _APPROX_NONE)
+    contours,_ = cv.findContours(im_gray, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     cv.drawContours(contour, contours, -1, (0,0,255), 3)
     #cv.imwrite("BB"+str(i)+".png",contour)
     
